06b6eec2d7737280b2d4c90d801346fb861aa779
Atul Pandey <86558585+Atulp1109@users.noreply.github.com>
2022-12-17 14:00:30 +0530

    complete

---
+++ client/src/helper/helper.js
@@ -0,0 +1,56 @@
+import _ from 'lodash';
+
+export function getTotal(transaction,type){
+    let sum =_(transaction)
+                .groupBy("type")
+                .map((objs,key)=>{
+                  if(!type)  return _.sumBy(objs,'amount');
+                  return {
+                    'type':key,
+                    'color':objs[0].color,
+                    'total':_.sumBy(objs,'amount')
+                  }
+                })
+                .value()
+    return sum;
+}
+
+export function getLabels(transaction){
+    let typeTotal=getTotal(transaction,'type');
+    let total=_.sum(getTotal(transaction))
+
+    let percent=_(typeTotal)
+                    .map(objs=>_.assign(objs,{percent:(100*objs.total)/total}))
+                    .value()
+
+    return percent;
+   
+}
+
+
+export function chart_Data(transaction,custom){
+    let bg=_.map(transaction,a=>a.color)
+    bg=_.uniq(bg)
+    let dataValue=getTotal(transaction)
+    const config={
+        data:{
+          datasets: [{
+          
+            data: dataValue,
+            backgroundColor: bg,
+            hoverOffset: 4,
+            borderRadius:30,
+            spacing:10 
+          }]
+        },
+        options:{
+          cutout:115
+    
+        }
+      }
+      return custom??config;
+}
+
+export function getTotalAmount(transaction){
+    return _.sum(getTotal(transaction))
+}
