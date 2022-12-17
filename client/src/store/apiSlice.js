f5381584dafe000367788e6b7582fce3df831775
Atul Pandey <86558585+Atulp1109@users.noreply.github.com>
2022-12-17 14:31:41 +0530

    new point

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -1,6 +1,6 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const baseURi='http://localhost:8080';
+const baseURi='https://expense-tracker-8l9p-main-nbfxwaoweq-wm.a.run.app/';
 export const apiSlice = createApi({
     baseQuery:fetchBaseQuery({
         baseUrl:baseURi
